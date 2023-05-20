6ab3a2079bfd16d609e4843c377f5c6031029cf0
Umut Can GÜR <umutcangurr@gmail.com>
2023-05-20 19:14:10 +0300

    A

--- FireBalls/Assets/Scripts/Umut Script/WindowQuestPointer.cs
+++ FireBalls/Assets/Scripts/Umut Script/WindowQuestPointer.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class WindowQuestPointer : MonoBehaviour
 {
+    //[SerializeField] private Camera uiCamera;
     private Vector3 targetPosition;
     private RectTransform pointerRectTransform;
 
@@ -26,6 +27,21 @@ public class WindowQuestPointer : MonoBehaviour
         Vector3 targetPositionScreenPoint = Camera.main.WorldToScreenPoint(targetPosition);
         bool isOffScreen = targetPositionScreenPoint.x <= 0 || targetPositionScreenPoint.x >= Screen.width || targetPositionScreenPoint.y <= 0 || targetPositionScreenPoint.y >= Screen.height;
         Debug.Log(isOffScreen + " " + targetPositionScreenPoint);
+
+        if(isOffScreen)
+        {
+            Vector3 cappedTargetScreenPosition = targetPositionScreenPoint;
+            if (cappedTargetScreenPosition.x <= 0) cappedTargetScreenPosition.x = 0f;
+            if (cappedTargetScreenPosition.x >= Screen.width) cappedTargetScreenPosition.x = Screen.width;
+            if (cappedTargetScreenPosition.y <= 0) cappedTargetScreenPosition.y = 0f;
+            if (cappedTargetScreenPosition.y >= Screen.height) cappedTargetScreenPosition.y = Screen.height;
+
+            Vector3 pointerWorldPosition = Camera.main.ScreenToWorldPoint(cappedTargetScreenPosition);
+            pointerRectTransform.position = pointerWorldPosition;
+            pointerRectTransform.localPosition = new Vector3(pointerRectTransform.localPosition.x, pointerRectTransform.localPosition.y, 0f); // Konrol et
+
+
+        }
 
     }
 }
