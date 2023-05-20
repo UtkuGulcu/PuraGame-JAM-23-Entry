71644287c928fe1b9316f5c2d18405b9a5935478
Umut Can GÜR <umutcangurr@gmail.com>
2023-05-20 20:41:55 +0300

    A

--- FireBalls/Assets/Scripts/Umut Script/WindowQuestPointer.cs
+++ FireBalls/Assets/Scripts/Umut Script/WindowQuestPointer.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WindowQuestPointer : MonoBehaviour
 {
     [SerializeField] private Camera uiCamera;
+    [SerializeField] private Sprite arrowSprite;
+    [SerializeField] private Sprite crossSprite;
     private Vector3 targetPosition;
     private RectTransform pointerRectTransform;
+    private Image pointerImage;
 
     private void Awake()
     {
         targetPosition = new Vector3(38,2,-1.5f);
         pointerRectTransform = transform.Find("Pointer").GetComponent<RectTransform>();
+        pointerImage = transform.Find("Pointer").GetComponent<Image>();
 
     }
 
@@ -24,24 +29,61 @@ public class WindowQuestPointer : MonoBehaviour
         float angle = Helpers.GetAngleFromVectorFloat(dir);
         pointerRectTransform.localEulerAngles = new Vector3(0,0,angle);
 
+        float borderSize = 100f;
+
         Vector3 targetPositionScreenPoint = Camera.main.WorldToScreenPoint(targetPosition);
-        bool isOffScreen = targetPositionScreenPoint.x <= 0 || targetPositionScreenPoint.x >= Screen.width || targetPositionScreenPoint.y <= 0 || targetPositionScreenPoint.y >= Screen.height;
+        bool isOffScreen = targetPositionScreenPoint.x <= borderSize || targetPositionScreenPoint.x >= Screen.width - borderSize || targetPositionScreenPoint.y <= borderSize || targetPositionScreenPoint.y >= Screen.height - borderSize;
         Debug.Log(isOffScreen + " " + targetPositionScreenPoint);
 
         if(isOffScreen)
         {
+            RotatePointerTowardsTargetPosition();
+            pointerImage.sprite = arrowSprite;
             Vector3 cappedTargetScreenPosition = targetPositionScreenPoint;
-            if (cappedTargetScreenPosition.x <= 0) cappedTargetScreenPosition.x = 0f;
-            if (cappedTargetScreenPosition.x >= Screen.width) cappedTargetScreenPosition.x = Screen.width;
-            if (cappedTargetScreenPosition.y <= 0) cappedTargetScreenPosition.y = 0f;
-            if (cappedTargetScreenPosition.y >= Screen.height) cappedTargetScreenPosition.y = Screen.height;
+            if (cappedTargetScreenPosition.x <= borderSize) cappedTargetScreenPosition.x = borderSize;
+            if (cappedTargetScreenPosition.x >= Screen.width - borderSize) cappedTargetScreenPosition.x = Screen.width - borderSize;
+            if (cappedTargetScreenPosition.y <= borderSize) cappedTargetScreenPosition.y = borderSize;
+            if (cappedTargetScreenPosition.y >= Screen.height - borderSize) cappedTargetScreenPosition.y = Screen.height - borderSize;
 
             Vector3 pointerWorldPosition = uiCamera.ScreenToWorldPoint(cappedTargetScreenPosition);
             pointerRectTransform.position = pointerWorldPosition;
-            pointerRectTransform.localPosition = new Vector3(pointerRectTransform.localPosition.x, pointerRectTransform.localPosition.y, 0f); // Konrol et
+            pointerRectTransform.localPosition = new Vector3(pointerRectTransform.localPosition.x, pointerRectTransform.localPosition.y, 0f); 
 
 
         }
+
+        else
+        {
+            pointerImage.sprite = crossSprite;
+            Vector3 pointerWorldPosition = uiCamera.ScreenToWorldPoint(targetPositionScreenPoint);
+            pointerRectTransform.position = pointerWorldPosition;
+            pointerRectTransform.localPosition = new Vector3(pointerRectTransform.localPosition.x, pointerRectTransform.localPosition.y, 0f);
+            pointerRectTransform.localEulerAngles = Vector3.zero;
+
+        }
+
+
+    }
+
+    private void RotatePointerTowardsTargetPosition()
+    {
+        Vector3 toPosition = targetPosition;
+        Vector3 fromPosition = Camera.main.transform.position;
+        fromPosition.z = 0f;
+        Vector3 dir = (toPosition - fromPosition).normalized;
+        float angle = Helpers.GetAngleFromVectorFloat(dir);
+        pointerRectTransform.localEulerAngles = new Vector3(0, 0, angle); 
+    }
+
+    public void Hide() //Doldurduðumuz þeker makinasýný iþaretlemeyi býraktýr.
+    {
+        gameObject.SetActive(false);
+
+    }
+
+    public void Show(Vector3 targetPosition) // Azalmýþ þeker makinasýnýn konumunu parametre olarak göndericez.
+    {
+        gameObject.SetActive(true);
 
     }
 }
