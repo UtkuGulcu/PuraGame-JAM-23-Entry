2f92a9a000bcfc76aba3083f18e306eff4f1d73b
Büşra Çetinkaya <80969758+busracetinkayaaa@users.noreply.github.com>
2023-05-21 12:33:46 +0300

    commit

--- FireBalls/Assets/Scripts/Busra Script/Audio.cs
+++ FireBalls/Assets/Scripts/Busra Script/Audio.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SoundController : MonoBehaviour
+public class Audio : MonoBehaviour
 {
-  
+    public AudioSource footstepSound;
+    public AudioSource carSound;
+    public AudioSource scoreSound;
 }
